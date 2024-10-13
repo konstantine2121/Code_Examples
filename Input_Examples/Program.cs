64e61fedd6bc77d2fd98b8a39cb7cfd82b3041c7
konstantine2121 <kach89kach89@gmail.com>
2024-10-13 17:21:39 +0300

    Add Input_Examples project

---
+++ Input_Examples/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Input_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Привет мир!");
+        }
+    }
+}
