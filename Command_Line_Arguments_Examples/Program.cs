e1c595ffc702544b2dd525c11b2f3667c3d10539
konstantine2121 <kach89kach89@gmail.com>
2024-10-13 19:30:43 +0300

    Add Input_Examples

---
+++ Command_Line_Arguments_Examples/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Command_Line_Arguments_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Привет Неизвестный пользователь!");
+            }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine($"Привет {args[0]}");
+            }
+            else
+            {
+                string name = args[0];
+                string stringAge = args[1];
+                int age = 0;
+
+                Console.WriteLine($"Привет {name}");
+
+                // try
+                // {
+                //     age = int.Parse(stringAge);// Небезопасный (может выбросить исключение)
+                // }
+                // catch 
+                // {
+                //     age = -1;
+                // }
+
+                bool correctAge = int.TryParse(stringAge, out age);// Безопасный (в случае провала вернет false, age = 0)
+
+                if (correctAge)
+                {
+                    Console.WriteLine($"{name}: полных лет {age}");
+                }
+
+                else
+                {
+                    Console.WriteLine("Возраст указан неверно!");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- Input_Examples/Program.cs
+++ Input_Examples/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Command_Line_Arguments_Examples
+﻿namespace Input_Examples
 {
     internal class Program
     {
@@ -7,48 +7,118 @@
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.InputEncoding = System.Text.Encoding.UTF8;
 
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Демонстрация ввода/вывода данных");
 
-            if (args.Length == 0)
+            PlayerCreator creator = new PlayerCreator();
+            PlayerPrinter printer = new PlayerPrinter();
+            List<Player> players = new List<Player>();
+
+            int numberOfPlayers = 2;
+
+            for(int i=0; i < numberOfPlayers; i++)
             {
-                Console.WriteLine("Привет Неизвестный пользователь!");
+                players.Add(creator.Create());
+                Console.WriteLine();
             }
-            else if (args.Length == 1)
+
+            Console.WriteLine(PlayerPrinter.TableFormat, "Имя", "Уровень");
+            Console.WriteLine(PlayerPrinter.TableFormat, "---", "---");
+
+            for (int i = 0; i < players.Count; i++)
             {
-                Console.WriteLine($"Привет {args[0]}");
+                printer.Print(players[i]);
             }
-            else
+        }
+
+        class Player
+        {
+            public string Name;
+            public int Level;
+        }
+
+        class PlayerPrinter
+        {
+            public const string TableFormat = "| {0, -20} | {1, 10} |";
+
+            /// <exception cref="ArgumentNullException"></exception>
+            public void Print(Player player)
             {
-                string name = args[0];
-                string stringAge = args[1];
-                int age = 0;
-
-                Console.WriteLine($"Привет {name}");
-
-                // try
-                // {
-                //     age = int.Parse(stringAge);// Небезопасный (может выбросить исключение)
-                // }
-                // catch 
-                // {
-                //     age = -1;
-                // }
-
-                bool correctAge = int.TryParse(stringAge, out age);// Безопасный (в случае провала вернет false, age = 0)
-
-                if (correctAge)
+                if (player == null)
                 {
-                    Console.WriteLine($"{name}: полных лет {age}");
+                    //Console.WriteLine($"{nameof(player)} не может быть null");
+                    throw new ArgumentNullException(nameof(player));
                 }
 
-                else
-                {
-                    Console.WriteLine("Возраст указан неверно!");
-                }
+                string message = string.Format(TableFormat, player.Name, player.Level);
+                Console.WriteLine(message);
+
+                //Console.WriteLine(TableFormat, player.Name, player.Level);
+                //под капотом вызывает string.Format(TableFormat, player.Name, player.Level);
+
+            }
+        }
+
+        class PlayerCreator
+        {
+            public Player Create()
+            {
+                string name = InputName();
+                int level = InputLevel();
+
+                Player player = new Player(); 
+                //new Player(){ Name = name, Level = level }; //Инициализаторы (новая версия языка)
+                player.Name = name;
+                player.Level = level;
+
+                return player;
             }
 
+            private string InputName()
+            {
+                bool correct = false;
+                string input = string.Empty;
 
-            Console.ReadLine();
+                while (!correct) 
+                {
+                    Console.Write("Введите имя: ");
+                    input = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        Console.WriteLine("Имя должно влючать в себя значимые символы.");
+                    }
+                    else 
+                    {
+                        correct = true;
+                    }
+                }
+                
+                return input;
+            }
+
+            private int InputLevel()
+            {
+                bool correct = false;
+                string input = string.Empty;
+                int level = 0;
+
+                while (!correct)
+                {
+                    Console.Write("Введите уровень: ");
+                    input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out level))
+                    {
+                        Console.WriteLine("Неверно указан уровень (ожидается целое число)");
+                    }
+                    else
+                    {
+                        correct = true;
+                    }
+                }
+
+                return level;
+            }
         }
     }
 }
