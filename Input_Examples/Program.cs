0b56c297b796785780662134654c11d9b4590532
konstantine2121 <kach89kach89@gmail.com>
2024-10-13 18:29:57 +0300

    Rename to  Command_Line_Arguments_Examples. Show multiwords args

--- Input_Examples/Program.cs
+++ Input_Examples/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Input_Examples
+﻿namespace Command_Line_Arguments_Examples
 {
     internal class Program
     {
@@ -9,15 +9,44 @@
 
             Console.ForegroundColor = ConsoleColor.Green;
 
-            if (args.Length > 0)
-            {
-                //Console.WriteLine("Привет " + args[0]); Старая
-                Console.WriteLine($"Привет {args[0]}");// Интерполяция строк
-            }
-            else
+            if (args.Length == 0)
             {
                 Console.WriteLine("Привет Неизвестный пользователь!");
             }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine($"Привет {args[0]}");
+            }
+            else
+            {
+                string name = args[0];
+                string stringAge = args[1];
+                int age = 0;
+
+                Console.WriteLine($"Привет {name}");
+
+                // try
+                // {
+                //     age = int.Parse(stringAge);// Небезопасный (может выбросить исключение)
+                // }
+                // catch 
+                // {
+                //     age = -1;
+                // }
+
+                bool correctAge = int.TryParse(stringAge, out age);// Безопасный (в случае провала вернет false, age = 0)
+
+                if (correctAge)
+                {
+                    Console.WriteLine($"{name}: полных лет {age}");
+                }
+
+                else
+                {
+                    Console.WriteLine("Возраст указан неверно!");
+                }
+            }
+
 
             Console.ReadLine();
         }
--- Input_Examples/start.bat
+++ Input_Examples/start.bat
@@ -2,3 +2,8 @@
 rem Если требуется отключить режим отображения команд и при этом не выводить на экран строку самой команды echo, введите символ @echo off  перед командой
 chcp 65001
 Input_Examples Петр
+Input_Examples Петр 12
+Input_Examples Петр 12,4
+Input_Examples Петр" "Первый
+Input_Examples "Петр Первый"
+Input_Examples "Петр Первый" 15
